0a74f4a27efbd57eab8d436a16e49bf39218a291
Shivanshu-Verma <b22es010@iitj.ac.in>
2024-07-04 02:35:04 +0000

    update v2

--- src/constants/strings.ts
+++ src/constants/strings.ts
@@ -1,23 +1,23 @@
 class Strings {
   static telegram = "Telegram";
   static telegramLink = "https://telegram.me/TheCrusaderRed";
-  static telegramUsername = "Shivanshu";
+  static telegramUsername = "Bots";
 
   static github = "GitHub";
   static githubLink = "https://www.github.com/Shivanshu-Verma";
-  static githubUsername = "Shivanshu";
+  static githubUsername = "Coding";
 
   static linkedIn = "LinkedIn";
   static linkedInLink = "https://www.linkedin.com/in/verma-shivanshu";
-  static linkedInUsername = "Shivanshu";
+  static linkedInUsername = "Networking";
 
   static instagram = "Instagram";
   static instagramLink = "https://www.instagram.com/_shivanshuv";
-  static instagramUsername = "Shivanshu";
+  static instagramUsername = "Filters";
 
   static twitter = "Twitter";
   static twitterLink = "https://www.twitter.com/ShivanshuV_";
-  static twitterUsername = "Shivanshu";
+  static twitterUsername = "Hashtags";
 
   static email = "Email";
   static primaryEmailLink = "mailto:v2.shivanshu@gmail.com";
